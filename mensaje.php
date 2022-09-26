464ac3c9b0ab3e54768296c880e244b6541cc7d3
SantiagoABM <barbozamujica109@gmail.com>
2022-09-25 21:18:39 -0500

    Actualización

---
+++ mensaje.php
@@ -0,0 +1,15 @@
+<?php
+    $destinatario='barbozamujica109@gmail.com';
+
+    $nombre= 'Vale';
+    $asunto= 'Ya no quiero nada';
+    $mensaje= $_POST('mensaje');
+    $email= 'rukasarashina2017@gmail.com';
+
+    $header = 'Perdón Santiago';
+    $mensajeCompleto= $mensaje . "\nAtt: " . $nombre;
+    mail($destinatario, 'ya no quiero nada', $mensajeCompleto, $header);
+    echo "<script>alert('Está bien Vale ya me quedó claro :c')</script>";
+    echo "<script>setTimeout(\"location.href='index.html'\",1000)</script>')";
+
+?>
